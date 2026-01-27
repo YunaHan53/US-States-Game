1d2eb3ded55ab780d39e12c0ef25a8f7982c5418
YunaH <Yunahan53@gmail.com>
2026-01-27 12:43:29 -0500

    Refactored and cleaned up initial code.

--- main.py
+++ main.py
@@ -9,38 +9,35 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-#Create a new turtle to write text for name of state
-write_text = turtle.Turtle()
-write_text.penup()
-write_text.color("black")
-write_text.hideturtle()
+# Create a new turtle to write text for name of state
+text = turtle.Turtle()
+text.penup()
+text.hideturtle()
 
-#Read the 50_states.csv file and extract the coordinates
+#Read the 50_states.csv file and extract the state names
 us_data = pd.read_csv("50_states.csv")
-states = us_data["state"]
+states = us_data["state"].to_list()
+correct_guess = 0
 
-guessed_state_coord = {}
-correct_guesses = 0
-
-while correct_guesses <= len(states):
+while correct_guess <= len(states):
     #Prompt user to input any U.S. states name:
-    guess_state = screen.textinput(f"{correct_guesses}/{len(states)} States Correct","Please enter a state name:").title()
+    guess_state = screen.textinput(title=f"{correct_guess}/{len(states)} States Correct",
+                                   prompt="Please enter a state name:").title()
 
-    #Compare the input to the U.S. state data list for a match.
+    #Compare the input to the U.S. states data list for a match.
     for state in states:
-        if state == guess_state:
-            # When a match is found, get the coordinates from the 50_states.csv file and place the state name at that location
-            state_coordinates = us_data.loc[us_data["state"] == state]
-            guessed_state_coord[state] = (state_coordinates["x"].values[0], state_coordinates["y"].values[0])
-            # print(guessed_state_coord[state])
-            write_text.goto(guessed_state_coord[state])
-            write_text.write(f"{state}", align="center", font=("Arial", 8, "normal"))
-            correct_guesses += 1
+        if guess_state == state:
+            # When a match is found, get the coordinates from the file and place the state name at that location
+            state_data = us_data[us_data["state"] == guess_state]
+            state_coord = (state_data["x"].item(), state_data["y"].item())
+            text.goto(state_coord)
+            text.write(f"{state}", align="center", font=("Arial", 7, "normal"))
+            correct_guess += 1
 
     #Terminate the game once all the states are filled in.
-    if correct_guesses == len(states):
-        write_text.goto(0,0)
-        write_text.write(f"Congratulations! You named all the 50 U.S. States!", align="center", font=("Arial", 24, "normal"))
+    if correct_guess == len(states):
+        text.goto(0,0)
+        text.write(f"Congratulations! You named all the 50 U.S. States!", align="center", font=("Arial", 24, "normal"))
         is_playing = False
         break
 
